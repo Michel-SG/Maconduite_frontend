e75cf8de609ff6df83cce04a74df81fd0c70851e
Michel SADEU <michelsadeungakou@gmail.com>
2024-03-30 16:34:28 +0100

    Implement user with uuid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {v4 as uuidv4} from 'uuid';
 function App() {
 
   const apifetch = async()=>{
+    let myuuid = uuidv4();
+    console.log(myuuid)
     const details = {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(
-        {email: "jacksonmiller@gmail.com",
-         password: "millerjack", 
+        {email: "jacksonmiller"+{myuuid}+"@gmail.com",
+         password: "millerjack"+{myuuid}, 
          first_name:"jackson",
          last_name : "miller"}),
     };
@@ -26,10 +28,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h2>Bonjour et bienvenue pour un test avec michel</h2>
+        <h2>Bonjour et bienvenue dans ce container de test avec michel</h2>
         <button className="square" onClick={apifetch}>
           button
         </button>
